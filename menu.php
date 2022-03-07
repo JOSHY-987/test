2e00ad8fd85c2ca396ea77afe85f420c72d0f728
JOSHY-987 <99046263+JOSHY-987@users.noreply.github.com>
2022-03-06 20:30:44 -0600

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style='background-color: #B983FF;'>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Tecnologia Web</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">Ejercicios</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="ejercicio1.php">Ejercicio 1.1</a></li>
+                        <li><a class="dropdown-item" href="ejercicio2.php">Ejercicio 2.1</a></li>
+                        <li><a class="dropdown-item" href="ejercicio3.php">Ejercicio 3.1</a></li>
+                        <li><a class="dropdown-item" href="ejercicio3_2.php">Ejercicio 3.2</a></li>
+                        <li><a class="dropdown-item" href="ejercicio4.php">Ejercicio 4.1</a></li>
+                        <li><a class="dropdown-item" href="ejercicio4_2.php">Ejercicio 4.2</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
